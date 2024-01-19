7b00dd98fc7321f82bc524a3324a8b4180e7c8bc
21120172 <nguyentuandat2k3cmg@gmail.com>
2024-01-19 13:34:56 +0700

    Init and implement server.js file

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const path = require('path');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const fs = require('fs');
+// read the ssl certificates from certificates/demo.crt and certificates/demo.key
+const privateKey = fs.readFileSync('certificates/demo.key', 'utf8');
+const certificate = fs.readFileSync('certificates/demo.crt', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+const https = require('https');
+const httpsServer = https.createServer(credentials, app);
+const port = process.env.PORT || 3000;
+httpsServer.listen(port, () => {
+  console.log(`Server is running on https://localhost:${port}`);
+});
