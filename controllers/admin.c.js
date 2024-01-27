d1b92220dac9ab98926f035e30bec4ac49c168e2
21120171 <nguyendinhanhvlqt@gmail.com>
2024-01-28 01:28:32 +0700

    Merge Huy and Anh

--- controllers/admin.c.js
+++ controllers/admin.c.js
@@ -27,81 +27,77 @@ module.exports = {
       user: req.user,
       categories: categories,
     });
- 
+
   },
 
   //POST /admin/category-management/edit
   postEditCategory: async (req, res, next) => {
     const { categoryName,
       categoryDescription,
-      categoryId} = req.body;
+      categoryId } = req.body;
     // console.log(categoryName);
 
-   try{
-    let result=await Category.editCategory(categoryId, categoryName, categoryDescription);
-    if (result)
-    {
-      res.json({success: true, message: 'Edit category successfully!'});
+    try {
+      let result = await Category.editCategory(categoryId, categoryName, categoryDescription);
+      if (result) {
+        res.json({ success: true, message: 'Edit category successfully!' });
+      }
+      else {
+        res.json({ success: false, message: 'Edit category failed!' });
+      }
     }
-    else {
-      res.json({success: false, message: 'Edit category failed!'});
+    catch (err) {
+      res.json({ success: false, message: 'Edit category failed!' });
     }
-  }
-  catch(err){
-    res.json({success: false, message: 'Edit category failed!'});
-  }
-},
-//POST /admin/category-management/create
+  },
+  //POST /admin/category-management/create
   postAddNewCategory: async (req, res, next) => {
     const { categoryName,
-      categoryDescription} = req.body;
-   try{
-    let category={
-      categoryname: categoryName,
-      categorydescription: categoryDescription
-    };
-    // console.log(category);
-    const newCategory=new Category(category);
-    // console.log(newCategory);
-    let result=await Category.addNewCategory(newCategory);
-    if (result)
-    {
-      res.json({success: true, message: 'Add Category successfully!'});
+      categoryDescription } = req.body;
+    try {
+      let category = {
+        categoryname: categoryName,
+        categorydescription: categoryDescription
+      };
+      // console.log(category);
+      const newCategory = new Category(category);
+      // console.log(newCategory);
+      let result = await Category.addNewCategory(newCategory);
+      if (result) {
+        res.json({ success: true, message: 'Add Category successfully!' });
+      }
+      else {
+        res.json({ success: false, message: 'Add Category failed!' });
+      }
     }
-    else {
-      res.json({success: false, message: 'Add Category failed!'});
+    catch (err) {
+      res.json({ success: false, message: 'Add Category failed!' });
     }
-  }
-  catch(err){
-    res.json({success: false, message: 'Add Category failed!'});
-  }
 
-},
-//POST /admin/category-management/delete
+  },
+  //POST /admin/category-management/delete
   postDeleteCategory: async (req, res, next) => {
-    const { categoryId} = req.body;
-   try{
+    const { categoryId } = req.body;
+    try {
 
-    const productCount=await dbCategory.checkCategoryHasExistProduct(categoryId);
-    if (productCount>0)
-    {
-      res.json({success: false, message: 'Cannot delete category because it has products.'});
-      return;
+      const productCount = await dbCategory.checkCategoryHasExistProduct(categoryId);
+      if (productCount > 0) {
+        res.json({ success: false, message: 'Cannot delete category because it has products.' });
+        return;
+      }
+      let result = await Category.deleteCategory(categoryId);
+      if (result) {
+        res.json({ success: true, message: 'Delete category successfully!' });
+      }
+      else {
+        res.json({ success: false, message: 'Delete category failed!1' });
+      }
     }
-    let result=await Category.deleteCategory(categoryId);
-    if (result)
-    {
-      res.json({success: true, message: 'Delete category successfully!'});
+    catch (err) {
+      res.json({ success: false, message: 'Delete category failed!2' });
     }
-    else {
-      res.json({success: false, message: 'Delete category failed!1'});
-    }
-  }
-  catch(err){
-    res.json({success: false, message: 'Delete category failed!2'});
-  }
-}
-  
+  },
+
   // GET /sales?mode=
   getSales: async (req, res, next) => {
     let mode = req.query.mode;
@@ -142,7 +138,6 @@ module.exports = {
 
     if (from && to) {
       const data = await Order.countOrdersByCategories(from, to);
-      console.log(data);
       res.json(data);
     }
   },
--- routers/admin.r.js
+++ routers/admin.r.js
@@ -2,15 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 const multer = require('multer');
-const upload=multer();
+const upload = multer();
 
 router.use(express.json());
-router.use(express.urlencoded({extended: true}));
+router.use(express.urlencoded({ extended: true }));
 const controllers = require('../controllers/admin.c');
 
+// For statistics charts
 router.get('/', controllers.getDashboard);
+router.get('/sales/products', require('../controllers/admin.c').getSales);
+router.get('/sales/categories', require('../controllers/admin.c').getSalesByCategories);
 
 
+// For category management
 router.get('/mangement/category', controllers.getCategoryManagement);
 
 router.post('/category-management/edit', controllers.postEditCategory);
@@ -19,12 +23,7 @@ router.post('/category-management/create', controllers.postAddNewCategory);
 
 router.post('/category-management/delete', controllers.postDeleteCategory);
 
+// For product management
 router.get('/management/products', require('../controllers/admin-products.c').manageProducts);
 
-// for statistics
-router.get('/sales/products', require('../controllers/admin.c').getSales);
-router.get('/sales/categories', require('../controllers/admin.c').getSalesByCategories);
-
-
 module.exports = router;
-
